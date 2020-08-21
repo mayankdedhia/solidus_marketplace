591dd03dbd1f7631ef811c3b7146e89257ab1a89
Mayank Dedhia <mayank.dedhia@systumbdi.com>
2020-08-21 09:33:46 +0530

    Refactor supplier_total to supplier_total_commissions to make it more meaningful

--- app/decorators/models/solidus_marketplace/spree/order_decorator.rb
+++ app/decorators/models/solidus_marketplace/spree/order_decorator.rb
@@ -8,7 +8,7 @@ module SolidusMarketplace
         base.has_many :suppliers, through: :stock_locations
       end
 
-      def supplier_total(user_or_supplier)
+      def supplier_total_commissions(user_or_supplier)
         supplier = user_or_supplier.is_a?(::Spree::Supplier) ? user_or_supplier : user_or_supplier.supplier
         shipments = self.shipments.by_supplier(supplier)
         commissions = shipments.map(&:supplier_commission_total)
@@ -19,8 +19,8 @@ module SolidusMarketplace
         suppliers.map do |s|
           {
             name: s.name,
-            earnings: self.supplier_total(s),
-            paypal_email: s.paypal_email
+            earnings: self.supplier_total_commissions(s),
+            paypal_email: s.paypal_email,
           }
         end
       end
